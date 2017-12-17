2350c4446cc1d6bca18d9d52f417eb7786c73679
MGF1998 <nikithekid@hotmail.de>
2017-12-17 10:12:24 +0100

    ring ring socket hang up

--- 2017/getinput.js
+++ 2017/getinput.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const url = require("url");
+let cookie = fs.readFileSync("./sekrit/cookie.txt");
 const readline = require("readline");
 const consoleinput = readline.createInterface(process.stdin,process.stdout);
 consoleinput.question("Enter day you wish to grab input from.",(num)=>{
@@ -7,12 +7,15 @@ consoleinput.question("Enter day you wish to grab input from.",(num)=>{
     let request = http.request({
         host:`www.adventofcode.com`,
         path:`/2017/day/${num}/input`,
-        headers:{"Cookie":"session=ayylmao"}
+        headers:{"Cookie":`session=${cookie}`}
     },(req)=>{
+        console.log(`status ${req.statusCode}`);
         req.on("data",(data)=>{
+            console.log(data);
             fs.writeFile(`inputs/day${num}.txt`,data.toString(),(err)=>{
                 if (err) throw err; else console.log("it's done.");
             });
         });
     });
+    request.on("error",err=>console.log(`Request encountered an error\n${err}`))
 });
