16bb713325e73d54b19835b4aba68aae29bbd493
MGF1998 <nikithekid@hotmail.de>
2017-12-17 09:54:59 +0100

    i have no idea what i'm doing

--- 2017/getinput.js
+++ 2017/getinput.js
@@ -1,13 +1,18 @@
 const fs = require("fs");
+const url = require("url");
 const readline = require("readline");
 const consoleinput = readline.createInterface(process.stdin,process.stdout);
 consoleinput.question("Enter day you wish to grab input from.",(num)=>{
     const http = require("http");
-    let request = http.get(`http://adventofcode.com/2017/day/${num}/input`,(req)=>{
+    let request = http.request({
+        host:`www.adventofcode.com`,
+        path:`/2017/day/${num}/input`,
+        headers:{"Cookie":"session=ayylmao"}
+    },(req)=>{
         req.on("data",(data)=>{
             fs.writeFile(`inputs/day${num}.txt`,data.toString(),(err)=>{
                 if (err) throw err; else console.log("it's done.");
             });
         });
     });
-})
+});
