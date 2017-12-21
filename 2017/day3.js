64163fa847d604547f489eecfab7045dab207024
MGF1998 <nikithekid@hotmail.de>
2017-12-21 12:24:57 +0100

    aaaa

---
+++ 2017/day3.js
@@ -0,0 +1,12 @@
+/*part 1
+Imagine a spiral of death that starts with 1 right at the center. Now take a step to the right, and spiral around counter-clockwise, adding 1 for each step.
+you'll get some meme like this:
+17  16  15  14  13
+18   5   4   3  12
+19   6   1   2  11
+20   7   8   9  10
+21  22  23---> ..
+now, get the manhattan distance from an input field to the field labeled 1.
+
+what have i done to deserve this
+*/
